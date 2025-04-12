4b5f602ab06ec9c242c97d25966f5aba3481bc39
Bhairavnath-Bhuse <bhairavnathbhuse@gmail.com>
2025-04-12 22:47:40 +0530

    first commit

---
+++ client/admin/js/script.js
@@ -0,0 +1,34 @@
+function loadDashboard(page) {
+    fetch(`././components/Dashboard/${page}`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(err => {
+            document.getElementById('main-content').innerHTML = '<p class="text-danger">Failed to load content.</p>';
+        });
+}
+
+function loadPageNavbar(page) {
+    fetch(`./components/Navbar/${page}`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(err => {
+            document.getElementById('main-content').innerHTML = '<p class="text-danger">Failed to load content.</p>';
+        });
+}
+
+
+function loadPageComponents(filename, targetId = 'main-content') {
+    fetch(`./components/${filename}`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(targetId).innerHTML = data;
+        })
+        .catch(err => {
+            console.error("Failed to load:", filename, err);
+            document.getElementById(targetId).innerHTML = '<p class="text-danger">Failed to load content.</p>';
+        });
+}
